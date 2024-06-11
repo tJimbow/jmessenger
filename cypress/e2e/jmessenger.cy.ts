620a5339bb7f38a0ab93a13a54474d7de28a4433
Jean-Marie TRIOMPHE <jean-marie.triomphe@external.engie.com>
2024-06-11 12:00:08 +0200

    add component test for timeline

--- cypress/e2e/jmessenger.cy.ts
+++ cypress/e2e/jmessenger.cy.ts
@@ -40,6 +40,7 @@ describe("posting a message", () => {
 
     it("should display 'Message should not be empty' error when the user try to send a message with empty message then remove it while sending a message", () => {
         given_now_is("2019-01-01T14:01:00.000Z");
+        given_adding_message_should_return_201();
         when_i_visit_the_jmessenger_page();
         when_user_post_an_empty_message();
         then_it_should_display_error_message("Message should not be empty");
@@ -48,12 +49,35 @@ describe("posting a message", () => {
     })
 })
 
+describe.only("view timeline", () => {
+    it("should view three messages for user Alice in her timeline", () => {
+        given_now_is("2019-01-01T14:02:00.000Z");
+        given_messages_for_alice([
+            {
+                id: "1", text: "my first message", postedAt: "2019-01-01T14:00:00.000Z"
+            },
+            {
+                id: "2", text: "my second message", postedAt: "2019-01-01T14:00:35.000Z"
+            },
+            {
+                id: "3", text: "my last message", postedAt: "2019-01-01T14:01:24.000Z"
+            },
+        ]);
+        when_i_visit_the_jmessenger_page();
+        then_alice_should_see_her_messages([
+            { position: "1", text: "my last message", publicationTime: "less than a minute ago" },
+            { position: "2", text: "my second message", publicationTime: "one minute ago" },
+            { position: "3", text: "my first message", publicationTime: "2 minutes ago" },
+        ]);
+    })
+});
+
 const when_i_visit_the_jmessenger_page = () => {
     cy.visit("/");
 }
 
 const then_i_should_see_messages_page_title = () => {
-    cy.contains(dataSelector('messages.title'), "Messages");
+    cy.contains(dataSelector('messages.title'), "Timeline");
 }
 
 const then_i_should_see_adding_message_form = () => {
@@ -93,5 +117,22 @@ const then_it_should_display_error_message = (message: string) => {
 
 const then_error_should_not_be_displayed = () => {
     cy.get(dataSelector('message.add.error')).should('not.exist');
+}
+
+interface MessageJson {
+    id: string;
+    text: string;
+    postedAt: string;
+}
+
+const given_messages_for_alice = (messages: MessageJson[]) => {
+    cy.intercept('GET', '/api/messages?author=Alice', { data: messages });
+}
+
+const then_alice_should_see_her_messages = (displayedMessage: { position: string; text: string; publicationTime: string; }[]) => {
+    displayedMessage.forEach(({ position, text, publicationTime }) => {
+        cy.get(dataSelector(`timeline.message.${position}.text`)).contains(text);
+        cy.get(dataSelector(`timeline.message.${position}.publicationTime`)).contains(publicationTime);
+    });
 }
 
--- src/infrastructure/secondary/MessageHttp.ts
+++ src/infrastructure/secondary/MessageHttp.ts
@@ -6,6 +6,10 @@ import type { HttpInstance } from '@/infrastructure/primary/HttpInstance';
 export class MessageHttp implements MessageRepository {
     constructor(private readonly http: HttpInstance) {
     }
+    
+    getMessagesByAuthor(author: string): Promise<Message[]> {
+        throw new Error("Method not implemented.");
+    }
 
     async save(message: Message): Promise<void> {
         await this.http.post("http://localhost:4173/api/add-message", MessageJson.of(message));
